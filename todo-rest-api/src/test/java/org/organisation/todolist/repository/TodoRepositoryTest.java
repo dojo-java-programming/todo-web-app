4ccd016b1373e9b416853a44c32524706a31368a
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-09-28 22:24:35 +0200

    Adds unit-test skeleton

---
+++ todo-rest-api/src/test/java/org/organisation/todolist/repository/TodoRepositoryTest.java
@@ -0,0 +1,12 @@
+package org.organisation.todolist.repository;
+
+import org.junit.Test;
+
+public class TodoRepositoryTest {
+
+	@Test
+	public void create() throws Exception {
+		// FIXME Add missing tests
+	}
+
+}
