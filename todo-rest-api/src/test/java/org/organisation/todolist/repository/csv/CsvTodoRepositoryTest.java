6c04292ef3b84097e4474314732cdeb7d1f40417
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-10-01 10:55:05 +0200

    Adds unit test

---
+++ todo-rest-api/src/test/java/org/organisation/todolist/repository/csv/CsvTodoRepositoryTest.java
@@ -0,0 +1,12 @@
+package org.organisation.todolist.repository.csv;
+
+import org.junit.Test;
+
+public class CsvTodoRepositoryTest {
+
+	@Test
+	public void create() throws Exception {
+		new CsvTodoRepository();
+	}
+
+}
